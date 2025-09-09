a9fc5e6ecc8e97990f7f0280c119075c829be41b
irish199718 <gudesirish2@gmail.com>
2025-09-09 15:02:33 +0800

    Added Student variable

---
+++ AlecerLife.js
@@ -0,0 +1,9 @@
+// Step 1: Variables
+const name = "Atasha Zacharine Alecer";
+const course = "BSIT";
+const yearLevel = "2nd year";
+
+console.log("Name:", name);
+console.log("Course:", course);
+console.log("Year Level:", yearLevel);
+
