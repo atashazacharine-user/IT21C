e827be59e5a43be01f049af9338e00cb5f144ed9
irish199718 <gudesirish2@gmail.com>
2025-09-10 14:10:58 +0800

    Added operators for daily routine

--- AlecerLife.js
+++ AlecerLife.js
@@ -1,4 +1,4 @@
-// Step 1: Variables
+//Variables
 const name = "Atasha Zacharine Alecer";
 const course = "BSIT";
 const yearLevel = "2nd year";
@@ -6,3 +6,13 @@ const yearLevel = "2nd year";
 console.log("Name:", name);
 console.log("Course:", course);
 console.log("Year Level:", yearLevel);
+
+
+//Operators
+const hoursSleep = 6;
+const hoursStudy = 5;
+const totalDailyHours = hoursSleep + hoursStudy;
+
+console.log("Hours of Sleep:", hoursSleep);
+console.log("Hours of Study:", hoursStudy);
+console.log("Total Daily Routine Hours:", totalDailyHours);
