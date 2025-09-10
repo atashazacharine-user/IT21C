8878339f9e8ca42150fa2b4fd06a1fa5b3e60605
Atasha Alecer <atashazacharine@gmail.com>
2025-09-10 14:43:58 +0800

    Added student variables

--- AlecerLifeFinal.js
+++ AlecerLifeFinal.js
@@ -6,3 +6,12 @@ const yearLevel = "2nd year";
 console.log("Name:", name);
 console.log("Course:", course);
 console.log("Year Level:", yearLevel);
+
+//Variables
+const name = "Atasha Zacharine Alecer";
+const course = "BSIT";
+const yearLevel = "2nd year";
+
+console.log("Name:", name);
+console.log("Course:", course);
+console.log("Year Level:", yearLevel);
