e55bcf9ec5bf24f1d22e1645b6e8c99aeefa9c60
Atasha Alecer <atashazacharine@gmail.com>
2025-09-10 14:45:17 +0800

    Added  condition for study hours

--- AlecerLifeFinal.js
+++ AlecerLifeFinal.js
@@ -15,3 +15,11 @@ const yearLevel = "2nd year";
 console.log("Name:", name);
 console.log("Course:", course);
 console.log("Year Level:", yearLevel);
+
+//Conditions
+
+if (hoursStudy >= 5) {
+  console.log("Great! You studied enough today.");
+} else {
+  console.log("You need to study more.");
+}
